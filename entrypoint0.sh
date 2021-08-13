5be199c3f077aef244022b5dc691d4f5005fd128
yzqiang666 <yzqiang6@gmail.com>
2021-08-14 01:00:02 +0800

    Update entrypoint0.sh

--- entrypoint0.sh
+++ entrypoint0.sh
@@ -83,6 +83,7 @@ sed -e "/^#/d"\
     
     
 [ ! "${NGINX_CONF_URL}" == "" ] && wget -O download1.tmp "$NGINX_CONF_URL"
+if [ -s download1.tmp ] && wget -O download1.tmp "$NGINX_CONF_URL"
 if [ -s download1.tmp ] && [ ! "`grep \"worker_processes\" download1.tmp`" == "" ] ; then
   cp download1.tmp /tmp/nginx.conf
   echo "Download from url ${NGINX_CONF_URL} file success." 
