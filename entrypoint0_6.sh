27bac3b78d96ff7c9a8b104e89f2825859ecb8a2
yzqiang666 <yzqiang6@gmail.com>
2021-08-14 11:49:38 +0800

    Update and rename entrypoint0_5.sh to entrypoint0_6.sh

--- entrypoint0_6.sh
+++ entrypoint0_6.sh
@@ -80,11 +80,8 @@ server {
     listen       [::]:${PORT};
     server_name  baidu.ggcloud.tk;
     location / {
-        index  index.html index.htm index.jsp;
+#        index  index.html index.htm index.jsp;
         proxy_pass https://www.baidu.com/;
-#        proxy_set_header User-Agent $http_user_agent;
-#        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; 
         proxy_redirect https://www.baidu.com/ https://baidu.ggcloud.tk/;
         
     }    
