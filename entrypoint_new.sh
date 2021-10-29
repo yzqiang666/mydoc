cd4df059e5f455977d953208e5dbca2bcee61785
yzqiang666 <yzqiang6@gmail.com>
2021-10-29 20:52:03 +0800

    Update entrypoint_new.sh

--- entrypoint_new.sh
+++ entrypoint_new.sh
@@ -177,7 +177,7 @@ echo rclone serve  webdav $CLOUDNAME$CLOUDPATH --addr :1888 $UU  $RCLONE_ARGUMEN
 rclone serve  webdav $CLOUDNAME$CLOUDPATH --addr :1888 $UU  $RCLONE_ARGUMENT &
 #rclone serve  webdav $CLOUDNAME$CLOUDPATH --addr :1888   $RCLONE_ARGUMENT &
 echo gost  -L="ss+ws://$ENCRYPT:$PASSWORD@:2334?host=${AppName}.herokuapp.com&mux=5&path=/gost"
-gost  -L="ss+ws://chacha20-ietf-poly1305:yzqyzq123@:2334?host=${AppName}.herokuapp.com&mux=5&path=/gost" &
+gost  -L="ss+ws://rc4-md5:yzqyzq123@:2334?host=${AppName}.herokuapp.com&mux=5&path=/gost" &
 
 cp /tmp/nginx.conf /etc/nginx/nginx.conf
 nginx -t -c /tmp/nginx.conf
