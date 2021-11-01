6efda8d2a85105f8bcc02177124b5bae337541e4
yzqiang666 <yzqiang6@gmail.com>
2021-11-01 22:08:55 +0800

    Update entrypoint_new.sh

--- entrypoint_new.sh
+++ entrypoint_new.sh
@@ -191,14 +191,15 @@ fi
 cat cache/config.json
 PP=${PORT}
 export PORT=33001
-npm config set registry https://registry.npm.taobao.org
-npm install n -g
-n stable
-npm install n -g
-n stable
-PATH="$PATH"
-node -v
-npm start &
+#npm config set registry https://registry.npm.taobao.org
+#npm install n -g
+#n stable
+#npm install n -g
+#n stable
+#PATH="$PATH"
+#node -v
+#npm start &
+/usr/local/bin/node app.js &
 export PORT=${PP}
 echo ==================================================
 
