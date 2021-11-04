6ef09973113aada75317c6b89cae95d1b0872dd1
yzqiang666 <yzqiang6@gmail.com>
2021-11-04 18:41:07 +0800

    Update entrypoint_new.sh

--- entrypoint_new.sh
+++ entrypoint_new.sh
@@ -211,7 +211,9 @@ PP=${PORT}
 export PORT=33001
 #/usr/local/bin/npm install
 #/usr/local/bin/npm audit fix --force
-npm install
+echo RRRRRRRRRRRRRRRRRR
+/usr/local/bin/npm install
+echo SSSSSSSSSSSS
 nohup npm start &
 export PORT=${PP}
 echo ================= $PORT   finish sharelist =================================
