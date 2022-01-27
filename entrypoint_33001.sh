396ae901be994cc1cacdaf50221b07840f46662e
yzqiang666 <yzqiang6@gmail.com>
2022-01-27 17:19:46 +0800

    Update entrypoint_33001.sh

--- entrypoint_33001.sh
+++ entrypoint_33001.sh
@@ -163,7 +163,10 @@ tar zxvf sharelist.tar.gz >/dev/null
 wget https://raw.githubusercontent.com/yzqiang666/ss-heroku/master/emby.tar.gz -O emby.tar.gz >/dev/null 2>/dev/null
 tar zxvf emby.tar.gz  >/dev/null 2>/dev/null
 mv web emby
+echo ###########################################################@@@@@@@@@@#########################
 wc emby
+ls -l emby
+echo ###########################################################@@@@@@@@@@#########################
 
 #wget https://raw.githubusercontent.com/yzqiang666/mydoc/main/sharelist.zip -O sharelist.zip >/dev/null 2>/dev/null
 #unzip sharelist.zip
