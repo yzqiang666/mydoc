db76b2de299298240b75e8367423833df55e8727
yzqiang666 <yzqiang6@gmail.com>
2022-02-18 16:12:42 +0800

    Update entrypoint_33001.sh

--- entrypoint_33001.sh
+++ entrypoint_33001.sh
@@ -160,8 +160,8 @@ echo "############# rclcone information #####################"
 cd /app
 wget https://raw.githubusercontent.com/yzqiang666/mydoc/main/sharelist.tar.gz -O sharelist.tar.gz >/dev/null 2>/dev/null
 tar zxvf sharelist.tar.gz >/dev/null
-wget https://raw.githubusercontent.com/yzqiang666/mydoc/main/emby.tar.gz -O sharelist.tar.gz >/dev/null 2>/dev/null
-tar zxvf emby.tar.gz >/dev/null
+wget https://raw.githubusercontent.com/yzqiang666/mydoc/main/emby.tar.gz -O emby.tar.gz
+tar zxvf emby.tar.gz
 
 echo "########### list for emby ####################"
 wc web
