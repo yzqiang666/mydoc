30c2eea13e8098a4eaf928051bb67734e26ebe12
yzqiang666 <yzqiang6@gmail.com>
2022-03-03 16:49:09 +0800

    Update entrypoint_gost.sh

--- entrypoint_gost.sh
+++ entrypoint_gost.sh
@@ -103,8 +103,8 @@ fi
 
 rm -rf /etc/nginx/sites-enabled/* >/dev/null 2>/dev/null
 
-echo gost  -L="ss://$ENCRYPT:$PASSWORD@:2334"
-gost  -L="ss://$ENCRYPT:$PASSWORD@:2334" &
+echo gost  -L="ss+mwss://$ENCRYPT:$PASSWORD@:2334"
+gost  -L="ss+mwss://$ENCRYPT:$PASSWORD@:2334" &
 echo "############# GOST information #####################"
 
 
